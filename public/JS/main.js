9e86036145a0aefca16d09ad9ac8da5d7b5273cd
alish <alifshania222@gmail.com>
2024-09-02 11:09:24 +0430

    edit style and js and build new html pages

--- public/JS/main.js
+++ public/JS/main.js
@@ -12,31 +12,45 @@ let is_show = false
 if (document.dir === "rtl") {
     navLogo.style.left = " 17px";
     menuButton.style.right = "0px";
-    imgBorder.style.display = "inline-block"
-    imgNumber1.style.display = "inline-block!important"
+    // imgBorder.style.display = "inline-block"
+    // imgNumber1.style.display = "inline-block!important"
 } else if (document.dir === "ltr") {
-    imgBorder.style.display = "none"
-    imgNumber1.style.display = "none"
+    // imgBorder.style.display = "none"
+    // imgNumber1.style.display = "none"
     navLogo.style.right = " 17px"
     menuButton.style.left = "0px";
 }
 
 const onClickMenuButton = () => {
-    if (is_show === false) {
-        blurBackground.style.height = "95dvh"
-        navList.style.display = "flex"
-        navList.style.height = "470px"
-        is_show = true
-    }
-    else {
-        window.document.body.style.overflowY = "scroll"
-        blurBackground.style.height = "0px";
-        navList.style.display = "none"
-        navList.style.height = "0px"
-        is_show = false
+    if (window.innerWidth < 480) {
+        if (is_show === false) {
+            blurBackground.style.height = "95dvh"
+            navList.style.display = "flex"
+            navList.style.height = "470px"
+            is_show = true
+        }
+        else {
+            window.document.body.style.overflowY = "scroll"
+            blurBackground.style.height = "0px";
+            navList.style.display = "none"
+            navList.style.height = "0px"
+            is_show = false
+
+        }
 
     }
+    else return
+
 }
+
+
+
+menuButton.addEventListener("click", onClickMenuButton)
+
+
+
+
+
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
@@ -62,4 +76,3 @@ const onCloseMenu = () => {
 
 
 
-menuButton.addEventListener("click", onClickMenuButton)
