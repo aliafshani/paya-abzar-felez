e44cb423462d167d68faceb50775b35db0b55c72
alish <alifshania222@gmail.com>
2024-09-02 09:57:52 +0430

    edit style

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,5 +1,7 @@
 let menuButton = document.querySelector(".nav-btn")
+let imgBorder = document.querySelector(".img-border")
 let blurBackground = document.querySelector(".blur-background ")
+let imgNumber1 = document.querySelector(".img-number1")
 let navList = document.querySelector(".nav-list")
 let navLogo = document.querySelector(".nav-logo")
 
@@ -10,7 +12,11 @@ let is_show = false
 if (document.dir === "rtl") {
     navLogo.style.left = " 17px";
     menuButton.style.right = "0px";
-} else {
+    imgBorder.style.display = "inline-block"
+    imgNumber1.style.display = "inline-block!important"
+} else if (document.dir === "ltr") {
+    imgBorder.style.display = "none"
+    imgNumber1.style.display = "none"
     navLogo.style.right = " 17px"
     menuButton.style.left = "0px";
 }
